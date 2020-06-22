ab79ecd558afa44cad7a4e1545d2047194ca4d28
jkaram <jamiekaram16@gmail.com>
2020-06-22 16:50:22 -0400

    Updates

---
+++ precache-manifest.caba7893b8610aa5839f34c9ac35eae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d33a4b145a7280ade16dc8174165f0a",
+    "url": "/mintbean-github/index.html"
+  },
+  {
+    "revision": "e9154f80ca4b1b0bf886",
+    "url": "/mintbean-github/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c9e1cf8dd8e913f42c4d",
+    "url": "/mintbean-github/static/js/2.f468bf5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mintbean-github/static/js/2.f468bf5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9154f80ca4b1b0bf886",
+    "url": "/mintbean-github/static/js/main.5a4be548.chunk.js"
+  },
+  {
+    "revision": "5fe84e916d4f18cb5e36",
+    "url": "/mintbean-github/static/js/runtime-main.4c571544.js"
+  }
+]);
