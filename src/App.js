e6363829bd221fb0b8434d5e6b830711e615b6ff
jkaram <jamiekaram16@gmail.com>
2020-06-22 15:09:50 -0400

    add loading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,193 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from "react";
+import debounce from "lodash/debounce";
+import styled, { createGlobalStyle } from "styled-components";
+import moment from "moment";
+import SearchBox from "./components/SearchBox";
+import Loading from "./components/loading";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    
+    min-height: 100vh;
+    margin: 0;
+    background-color: #fff;
+    background-image: url("https://www.transparenttextures.com/patterns/axiom-pattern.png");
+
+  }
+`;
 
 function App() {
+  const token = process.env.REACT_APP_GITHUB_API_TOKEN;
+  const [state, setState] = useState({
+    text: "",
+    results: [],
+    repos: [],
+    loading: false,
+  });
+
+  const { text, results, loading, repos } = state;
+
+  const [clear, setClear] = useState(false);
+
+  async function getUser(name) {
+    let response = await fetch(`https://api.github.com/users/${name}`);
+    let data = await response.json();
+    return data;
+  }
+
+  async function getUserRepos(name) {
+    let response = await fetch(`https://api.github.com/users/${name}/repos`);
+    let data = await response.json();
+    return data;
+  }
+
+  const updateText = (text) => {
+    setState((prevState) => ({ ...prevState, text }));
+  };
+
+  const clearResults = () => {
+    setState((prevState) => ({
+      ...prevState,
+      results: [],
+      repos: [],
+    }));
+    setClear(true);
+  };
+
+  const search = async (text) => {
+    setState((prevState) => ({
+      ...prevState,
+      loading: true,
+    }));
+
+    const githubUser = await getUser(text);
+    setState((prevState) => ({
+      ...prevState,
+      results: githubUser,
+    }));
+
+    const userRepos = await getUserRepos(text);
+    setState((prevState) => ({
+      ...prevState,
+      repos: userRepos,
+    }));
+
+    setTimeout(function () {
+      setState((prevState) => ({
+        ...prevState,
+        loading: false,
+      }));
+    }, 2100);
+  };
+
+  const debouncedSearch = useCallback(
+    debounce((text) => search(text), 3000),
+    []
+  );
+
+  console.log(state);
+  console.log(token, "here");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Header>GitHub User Lookup!</Header>
+      <SearchBox
+        text={text}
+        updateText={updateText}
+        debouncedSearch={debouncedSearch}
+        clearResults={clearResults}
+      />
+      <GlobalStyle />
+      {loading && <Loading />}
+
+      {!loading && results.login && (
+        <Profile>
+          <UserImg src={results.avatar_url} alt={results.login} />
+          <div>
+            <h1 style={{ fontSize: "18px", fontWeight: "bold" }}>
+              <a
+                href={results.html_url}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {results.login}
+              </a>
+            </h1>
+            Public Repos {results.public_repos}
+          </div>
+        </Profile>
+      )}
+      <Results>
+        {!loading &&
+          repos &&
+          repos
+            .sort((a, b) => (a.updated_at < b.updated_at ? 1 : -1))
+            .map((repo) => {
+              var date = moment(repo.updated_at);
+              var dateComponent = date.utc().format("YYYY-MM-DD");
+
+              return (
+                <Repo>
+                  <RepoTitle href={repo.html_url}>{repo.name}</RepoTitle>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      margin: "10px 0 0",
+                    }}
+                  >
+                    <div>{repo.language}</div>
+                    <date>{dateComponent}</date>
+                  </div>
+                </Repo>
+              );
+            })}
+      </Results>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const UserImg = styled.img`
+  border-radius: 50%;
+  max-width: 150px;
+  width: 100%;
+`;
+
+const Header = styled.h1``;
+
+const Profile = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 20px auto;
+  width: 60%;
+`;
+
+const Wrapper = styled.div`
+  max-width: 800px;
+  width: 100%;
+  margin: 0 auto;
+`;
+const Results = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  width: 100%;
+`;
+const Repo = styled.div`
+  display: inline-block;
+  max-width: 300px;
+  width: 100%;
+  height: 50px;
+  border: 1px solid #000;
+  padding: 5px 15px;
+  margin: 10px 0;
+  background: #fff;
+`;
+
+const RepoTitle = styled.a`
+  text-decoration: none;
+  color: #000;
+  font-weight: bold;
+`;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+const SearchBoxWrapper = styled.div`
+  display: flex;
+  width: 95%;
+  margin: 5% auto;
+  justify-content: center;
+`;
+
+const SearchInput = styled.input`
+  max-width: 500px;
+  width: 100%;
+  border-radius: 3px;
+  font-size: 1.5rem;
+  box-sizing: border-box;
+  height: 3rem;
+  border: 1px solid #ccc;
+  display: block;
+  padding: 2px 15px;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+`;
+
+const ClearButton = styled.button`
+  width: 100px;
+  border: 1px solid #ccc;
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+  border-left: 0;
+  background: white;
+  font-weight: bold;
+`;
+
+export default function SearchBox({
+  text,
+  debouncedSearch,
+  updateText,
+  clearResults,
+}) {
+  return (
+    <SearchBoxWrapper>
+      <SearchInput
+        id="text"
+        value={text}
+        autoComplete="off"
+        maxLength="50"
+        placeholder="Look-up a github user!"
+        onChange={(event) => {
+          updateText(event.target.value);
+          debouncedSearch(event.target.value);
+        }}
+      />
+      <ClearButton
+        onClick={() => {
+          clearResults();
+          updateText("");
+        }}
+        disabled={!text}
+      >
+        Clear
+      </ClearButton>
+    </SearchBoxWrapper>
+  );
+}
