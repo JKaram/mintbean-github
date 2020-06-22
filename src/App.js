05e1a761632b1cbc692b904c2dd44f4139e34d0d
jkaram <jamiekaram16@gmail.com>
2020-06-22 15:59:47 -0400

    cleanup warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import debounce from "lodash/debounce";
 import styled, { createGlobalStyle } from "styled-components";
 import moment from "moment";
